f1c44e6764e9336d72fae3853b2e874449345b6d
Mayfff <zakrevsky.d.s@gmail.com>
2024-04-15 17:47:32 +0300

    added function to calculate x

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -14,7 +14,54 @@ FONT = ("Arial", 18, "bold")
 BUTTONCOLOR = "#FFFF63"
 BUTTONFONT = ("Arial", 20)
 
-a, b, e = 0, 0, 0
+def method(A, B, epsilon):
+    arrayA.append(min(A, B))
+    arrayB.append(max(A, B))
+
+    if abs(B - A) < epsilon:
+        return (A + B) / 2
+
+    if f(B) * secondDerivativeF(B) < 0:
+        A, B = B, A
+
+    x = B - f(B) / derivativeF(B)
+    arrayX.append(x)
+
+    if abs(x - B) < epsilon:
+        return x
+
+    return method(A, x, epsilon)
+
+def secondDerivativeF(x):
+    return 6 * x
+
+def derivativeF(x):
+    return 3 * (x ** 2) + 10
+
+def f(x):
+    return x ** 3 + 10 * x - 9
+
+def enter():
+    global a, b, e
+    try:
+        a = float(aEntry.get().replace(',', '.'))
+        b = float(bEntry.get().replace(',', '.'))
+        e = float(eEntry.get().replace(',', '.'))
+    except ValueError:
+        messagebox.showerror("Помилка", "Введіть правильні значення")
+        return
+
+    if b <= a:
+        messagebox.showerror("Помилка", "b < a")
+        return
+
+    if f(a) * f(b) > 0:
+        messagebox.showerror("Помилка", "Неправильні межі")
+        return
+
+arrayA = []
+arrayB = []
+arrayX = []
 
 root = Tk()
 root.title("Головне вікно. Лабораторна №4")
@@ -39,12 +86,15 @@ Label(root, text="___" * 1000, bg=BG, fg="white", font=FONT).grid(row=6, columns
 
 aEntry = Entry(root, width=20)
 aEntry.grid(row=7, column=1, padx=10, pady=5, sticky="w")
-Label(root, text="Введіть A:", bg=BG, font=FONT).grid(row=7, column=0, padx=(0, 10), pady=5, sticky="e")
+Label(root, text="Введіть A:", bg=BG, font=FONT, fg="white").grid(row=7, column=0, padx=(0, 10), pady=5, sticky="e")
 bEntry = Entry(root, width=20)
 bEntry.grid(row=8, column=1, padx=10, pady=5, sticky="w")
-Label(root, text="Введіть B:", bg=BG, font=FONT).grid(row=8, column=0, padx=(0, 10), pady=5, sticky="e")
+Label(root, text="Введіть B:", bg=BG, font=FONT, fg="white").grid(row=8, column=0, padx=(0, 10), pady=5, sticky="e")
 eEntry = Entry(root, width=20)
 eEntry.grid(row=9, column=1, padx=10, pady=5, sticky="w")
-Label(root, text="Введіть ε:", bg=BG, font=FONT).grid(row=9, column=0, padx=(0, 10), pady=5, sticky="e")
+Label(root, text="Введіть ε:", bg=BG, font=FONT, fg="white").grid(row=9, column=0, padx=(0, 10), pady=5, sticky="e")
+
+enterButton = Button(root, text="Ввести", bg=BUTTONCOLOR, font=BUTTONFONT, command=enter)
+enterButton.grid(row=10, columnspan=2, padx=10)
 
 root.mainloop()
